63fc151b317226a6d76f28a03b8d68bb3fca8282
yatharth-7022 <yatharth7022@gmail.com>
2025-05-26 15:47:46 +0530

    navbar css change'

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FaHome, FaBriefcase, FaTools, FaProjectDiagram, FaEnvelope } from 'react-icons/fa'; // Import icons
-import './Navbar.css';
-import netflixLogo from '../images/logo-2.png';
-import blueImage from '../images/blue.png';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import {
+  FaHome,
+  FaBriefcase,
+  FaTools,
+  FaProjectDiagram,
+  FaEnvelope,
+} from "react-icons/fa"; // Import icons
+import "./Navbar.css";
+import netflixLogo from "../images/logo-2.png";
+import blueImage from "../images/blue.png";
 
 const Navbar: React.FC = () => {
   const location = useLocation();
@@ -17,8 +23,8 @@ const Navbar: React.FC = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const toggleSidebar = () => {
@@ -31,17 +37,27 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+      <nav className={`navbar ${isScrolled ? "scrolled" : ""}`}>
         <div className="navbar-left">
           <Link to="/" className="navbar-logo">
             <img src={netflixLogo} alt="Netflix" />
           </Link>
           <ul className="navbar-links">
-            <li><Link to="/browse">Home</Link></li>
-            <li><Link to="/work-experience">Professional</Link></li>
-            <li><Link to="/skills">Skills</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/contact-me">Hire Me</Link></li>
+            <li>
+              <Link to="/browse">Home</Link>
+            </li>
+            <li>
+              <Link to="/work-experience">Professional</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact-me">Hire Me</Link>
+            </li>
           </ul>
         </div>
         <div className="navbar-right">
@@ -51,24 +67,51 @@ const Navbar: React.FC = () => {
             <div></div>
             <div></div>
           </div>
-          <img src={profileImage} alt="Profile" className="profile-icon" onClick={() => { navigate('/browse') }} />
+          <img
+            src={profileImage}
+            alt="Profile"
+            className="profile-icon"
+            onClick={() => {
+              navigate("/browse");
+            }}
+          />
         </div>
       </nav>
 
       {/* Sidebar Overlay */}
-      <div className={`sidebar-overlay ${isSidebarOpen ? 'open' : ''}`} onClick={closeSidebar}></div>
+      <div
+        className={`sidebar-overlay ${isSidebarOpen ? "open" : ""}`}
+        onClick={closeSidebar}
+      ></div>
 
       {/* Sidebar (only visible on mobile) */}
-      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <div className="sidebar-logo">
-          <img src={netflixLogo} alt="Netflix Logo" />
-        </div>
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
         <ul>
-          <li><Link to="/browse" onClick={closeSidebar}><FaHome /> Home</Link></li>
-          <li><Link to="/work-experience" onClick={closeSidebar}><FaBriefcase /> Professional</Link></li>
-          <li><Link to="/skills" onClick={closeSidebar}><FaTools /> Skills</Link></li>
-          <li><Link to="/projects" onClick={closeSidebar}><FaProjectDiagram /> Projects</Link></li>
-          <li><Link to="/contact-me" onClick={closeSidebar}><FaEnvelope /> Hire Me</Link></li>
+          <li>
+            <Link to="/browse" onClick={closeSidebar}>
+              <FaHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/work-experience" onClick={closeSidebar}>
+              <FaBriefcase /> Professional
+            </Link>
+          </li>
+          <li>
+            <Link to="/skills" onClick={closeSidebar}>
+              <FaTools /> Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" onClick={closeSidebar}>
+              <FaProjectDiagram /> Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact-me" onClick={closeSidebar}>
+              <FaEnvelope /> Hire Me
+            </Link>
+          </li>
         </ul>
       </div>
     </>
