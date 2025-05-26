9d079f7a1a0deaf22cb4a1187e8848dc96724788
yatharth-7022 <yatharth7022@gmail.com>
2025-05-26 11:54:11 +0530

    completing contact me page

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,35 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import './ContactMe.css';
-import profilePic from '../images/sumanth.jpeg';
-import { FaEnvelope, FaPhoneAlt, FaCoffee, FaLinkedin } from 'react-icons/fa';
-import { ContactMe as IContactMe } from '../types';
-import { getContactMe } from '../queries/getContactMe';
+import React, { useEffect, useState } from "react";
+import "./ContactMe.css";
+import profilePic from "../images/sumanth.jpeg";
+import { FaEnvelope, FaPhoneAlt, FaCoffee, FaLinkedin } from "react-icons/fa";
+import { ContactMe as IContactMe } from "../types";
+import { getContactMe } from "../queries/getContactMe";
+interface UserProfile {
+  name: string;
+  title: string;
+  companyUniversity: string;
+  email: string;
+  phoneNumber: string;
+  linkedinLink: string;
+  summary: string;
+}
 
+const userData: UserProfile = {
+  name: "Yatharth Agarwal",
+  title: "Software Developer",
+  companyUniversity: "GoTrust | Bennett University",
+  email: "yatharth7022@gmail.com",
+  phoneNumber: "+91 7082287214",
+  summary:
+    "With strong experience in building responsive and dynamic user interfaces using React, Angular, JavaScript, TypeScript, and Tailwind CSS. Skilled in creating modern, accessible, and user-friendly web apps.",
+  linkedinLink: "https://www.linkedin.com/in/yatharth-agarwal-666787255/",
+};
 const ContactMe: React.FC = () => {
-
-  const [userData, setUserData] = useState<IContactMe>()
-
-  useEffect(() => {
-    async function fetchUserData() {
-      const data = await getContactMe();
-      setUserData(data);
-    }
-
-    fetchUserData();
-  }, []);
-
-  if (!userData) return <div>Loading...</div>;
-
   return (
     <div className="contact-container">
       <div className="linkedin-badge-custom">
-        <img src={profilePic} alt="Yatharth Agarwal" className="badge-avatar" />
+        <img src="/me.jpeg" alt="Yatharth Agarwal" className="badge-avatar" />
         <div className="badge-content">
           <h3 className="badge-name">{userData?.name}</h3>
           <p className="badge-title">{userData.title}</p>
-          <p className="badge-description">
-            {userData.summary}
-          </p>
+          <p className="badge-description">{userData.summary}</p>
           <p className="badge-company">{userData.companyUniversity}</p>
           <a
             href={userData.linkedinLink}
